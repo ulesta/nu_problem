1babaee5d7ab84b1357a1353383673e4ae2dbc7d
Justin Cuaresma <justincuaresma@gmail.com>
2016-04-06 12:19:18 -0400

    Added Decorator class which helps decorating a value to desired output. Added specs for Decorator

--- spec/decorator_spec.js
+++ spec/decorator_spec.js
@@ -1,3 +1,60 @@
-/**
- * Created by justin on 2016-04-06.
- */
+var Decorator = require('../lib/decorator');
+
+describe("Testing Decorator functionality", function() {
+    var initialVal = 100,
+        myDecorator = new Decorator();
+
+    it("it should return the original value if there are no Decorators", function() {
+        var output = myDecorator.decorate(initialVal);
+
+        expect(output).toEqual(initialVal);
+    });
+
+    it("it should add 13% tax", function() {
+        myDecorator.addDecoration('tax', function(value) {
+            return value * 1.13;
+        });
+        var output = myDecorator.decorate(initialVal);
+
+        expect(output).toEqual(initialVal * 1.13);
+    });
+
+    it("it should calculate final price based on markups", function() {
+        var myDecoratorPrice = new Decorator(),
+            myDecoratorMarkups = new Decorator();
+            initialVal = 1299.99;
+
+        var FLAT_MARKUP = 0.05,
+            NUM_WORKERS = 3,
+            WORKER_MARKUP = 0.012,
+            FOOD_MARKUP = 0.13;
+
+        myDecoratorMarkups.addDecoration('workerMarkup', (function() {
+            var markup = 0;
+            for (var i = 0; i < NUM_WORKERS; i++) {
+                markup += WORKER_MARKUP;
+            }
+
+            return function(value) {
+                return value + markup;
+            }
+        })());
+
+        myDecoratorMarkups.addDecoration('foodMarkup', function(value) {
+            return value + FOOD_MARKUP;
+        });
+
+        myDecoratorPrice.addDecoration('flatMarkup', function(value) {
+            return value * (1+FLAT_MARKUP);
+        });
+
+        myDecoratorPrice.addDecoration('additionalMarkups', function(value) {
+            return value * myDecoratorMarkups.decorate(1)
+        });
+
+        var output = Math.round(myDecoratorPrice.decorate(initialVal) * 100) / 100;
+
+        expect(output).toEqual(1591.58);
+    });
+
+});
--- spec/file_input_spec.js
+++ spec/file_input_spec.js
@@ -6,7 +6,15 @@ describe("Testing app reads from input", function() {
         expect(nupack.readInput).toThrow(new Error("Must supply file as argument!"));
     });
 
-    it("should have a first line: $1299.99", function() {
+    it("should have Line 1: $1299.99", function() {
+        expect(nupack.readInput('./spec/input/in1')[0]).toEqual("$1299.99");
+    });
+
+    it("should have Line 2: 3 people", function() {
+        expect(nupack.readInput('./spec/input/in1')[0]).toEqual("$1299.99");
+    });
+
+    it("should have Line 3: food", function() {
         expect(nupack.readInput('./spec/input/in1')[0]).toEqual("$1299.99");
     });
 
