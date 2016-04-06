8f27d14ee98b79df7f4e555fb36c6bc62225cbfc
Justin Cuaresma <justincuaresma@gmail.com>
2016-04-06 15:27:57 -0400

    Added extra test cases: check for correct estimate on file input, check for corner cases. Added Errors lookup table. Fixed bug where readInput calls validateAndSet() but does not bind this.

--- spec/nupack_spec.js
+++ spec/nupack_spec.js
@@ -1,63 +1,139 @@
-var nupack = require('../lib/nupack');
+var nupack = require('../lib/nupack'),
+    in1 = './spec/input/in1',
+    in2 = './spec/input/in2',
+    in3 = './spec/input/in3',
+    invalid1 = './spec/input/invalid1',
+    invalid2 = './spec/input/invalid2',
+    invalid3 = './spec/input/invalid3';
 
-describe("Testing app reads from input", function() {
 
-    it("it should throw an error", function() {
-        expect(nupack.readInput).toThrow(new Error("Must supply file as argument!"));
+describe('Testing app reads from input', function () {
+    it('it should throw an error', function () {
+        expect(nupack.readInput).toThrow(new Error('Must supply file as argument!'));
     });
 
-    it("should have Line 1: $1299.99", function() {
-        expect(nupack.readInput('./spec/input/in1')[0]).toEqual("$1299.99");
+    it('should have Line 1: $1299.99', function () {
+        expect(nupack.readInput(in1)[0]).toEqual('$1299.99');
     });
 
-    it("should have Line 2: 3 people", function() {
-        expect(nupack.readInput('./spec/input/in1')[1]).toEqual("3 people");
+    it('should have Line 2: 3 people', function () {
+        expect(nupack.readInput(in1)[1]).toEqual('3 people');
     });
 
-    it("should have Line 3: food", function() {
-        expect(nupack.readInput('./spec/input/in1')[2]).toEqual("food");
+    it('should have Line 3: food', function () {
+        expect(nupack.readInput(in1)[2]).toEqual('food');
     });
-
 });
 
-describe("App should produce correct estimate", function() {
-    it("should produce: 1591 based on sample input 1", function() {
+describe('App should produce correct estimate', function () {
+    it('should produce: 1591 based on sample input 1', function () {
         nupack.init({
             price: 1299.99,
             numWorkers: 3,
-            category: "food"
+            category: 'food'
         });
-        expect(nupack.getEstimate()).toEqual(1591.58);
+        expect(nupack.getEstimate()).toEqual('$1591.58');
     });
 
-    it("should produce: 6199.81 based on sample input 2", function() {
+    it('should produce: 6199.81 based on sample input 2', function () {
         nupack.init({
             price: 5432,
             numWorkers: 1,
-            category: "drugs"
+            category: 'drugs'
         });
-        expect(nupack.getEstimate()).toEqual(6199.81);
+        expect(nupack.getEstimate()).toEqual('$6199.81');
     });
 
-    it("should produce: 13707.63 based on sample input 3", function() {
+    it('should produce: 13707.63 based on sample input 3', function () {
         nupack.init({
             price: 12456.95,
             numWorkers: 4,
-            category: "books"
+            category: 'books'
         });
-        expect(nupack.getEstimate()).toEqual(13707.63);
+        expect(nupack.getEstimate()).toEqual('$13707.63');
     });
 
-    it("should produce: 13707 with new price Modification Floor", function() {
+    it('should produce: 13707 with new price Modification Floor', function () {
         nupack.init({
             price: 12456.95,
             numWorkers: 4,
-            category: "books"
+            category: 'books'
         });
-        nupack.addPriceModification("floor", function(value) {
+        nupack.addPriceModification('floor', function (value) {
             return Math.floor(value);
         });
-        expect(nupack.getEstimate()).toEqual(13707);
+        expect(nupack.getEstimate()).toEqual('$13707');
+    });
+});
+
+describe('App will read from file and produce correct output', function () {
+    it('should produce: 1591 based on sample input 1', function () {
+        nupack.init({ file: in1 });
+
+        expect(nupack.getEstimate()).toEqual('$1591.58');
     });
 
+    it('should produce: 6199.81 based on sample input 2', function () {
+        nupack.init({ file: in2 });
+        expect(nupack.getEstimate()).toEqual('$6199.81');
+    });
+
+    it('should produce: 13707.63 based on sample input 3', function () {
+        nupack.init({ file: in3 });
+        expect(nupack.getEstimate()).toEqual('$13707.63');
+    });
+});
+
+describe('App will throw Errors whenever appropriate', function () {
+    it('should throw Error: missing params', function () {
+        expect(function() {
+            return nupack.init({});
+        }).toThrow(new Error('Must have params object: { config (optional): {}, file: String } || { config(optional): {}, price: Number, numWorkers: Int, category: String }'));
+    });
+
+    it('should throw Error: invalid price', function () {
+        expect(function() {
+            return nupack.init({
+                file: invalid1
+            });
+        }).toThrow(new Error('Invalid price input!'));
+    });
+
+    it('should throw Error: invalid num of workers', function () {
+        expect(function() {
+            return nupack.init({
+                file: invalid2
+            });
+        }).toThrow(new Error('Invalid number of workers!'));
+    });
+
+    it('should throw Error: invalid category', function () {
+        expect(function() {
+            return nupack.init({
+                file: invalid3
+            });
+        }).toThrow(new Error('Category is empty!'));
+    });
+});
+
+describe('App throws Errors whenever supplied with negative inputs', function () {
+    it('should throw Error: invalid price input', function () {
+        expect(function() {
+            return nupack.init({
+                price: -12456.95,
+                numWorkers: 4,
+                category: 'books'
+            });
+        }).toThrow(new Error('Invalid price input!'));
+    });
+
+    it('should throw Error: invalid num of workers', function () {
+        expect(function() {
+            return nupack.init({
+                price: 12456.95,
+                numWorkers: -4,
+                category: 'books'
+            });
+        }).toThrow(new Error('Invalid number of workers!'));
+    });
 });
